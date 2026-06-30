fix scaling bug in crires echelle spectra, disentangle sigma clipping from continuum subtraction

sika/implementations/spectroscopy/crires/crires_echelle_loader.py
@@ -13,7 +13,7 @@ import pandas as pd
 
 __all__ = ["load_crires_echelle_spectrum", "CRIRESEchelleDataLoader"]
 
-def load_crires_echelle_spectrum(data_file: str, wavelength_file: str, wavelen_range: Tuple[float,float] | None, filter_size:int, filter_type:str, bp_sigma:float, masked_ranges:Optional[List[Tuple[float,float]]]=None, metadata=None) -> CRIRESEchelleSpectrum:
+def load_crires_echelle_spectrum(data_file: str, wavelength_file: str, wavelen_range: Tuple[float,float] | None, sigma_clip, remove_continuum, filter_size:int, filter_type:str, bp_sigma:float, masked_ranges:Optional[List[Tuple[float,float]]]=None, metadata=None) -> CRIRESEchelleSpectrum:
     """Load a CRIRES spectrum from file. Loaded spectra will be continuum subtracted and normalized.
 
     :param data_file: Path to science data
@@ -22,6 +22,10 @@ def load_crires_echelle_spectrum(data_file: str, wavelength_file: str, wavelen_r
     :type wavelength_file: str
     :param wavelen_range: If None, no crop performed. If tuple (start, stop), will crop to only include this wavelength range.
     :type wavelen_range: tuple
+    :param sigma_clip: whether to clean the continuum with a sigma clip of sigma=bp_sigma
+    :type sigma_clip: bool
+    :param remove_continuum: whether to remove the continuum using the specified filter
+    :type remove_continuum: bool
     :param filter_size: Size of the filter to apply
     :type filter_size: int
     :param filter_type: Type of the filter to apply - 'median' or 'gaussian'
@@ -47,7 +51,7 @@ def load_crires_echelle_spectrum(data_file: str, wavelength_file: str, wavelen_r
     flux = np.array(df['flux'])
     errors = np.array(df['err'])
     if wavelen_range is not None:
-        clip_indices = np.where((wlen >= wavelen_range[0]) & (wlen <= wavelen_range[1]))
+        clip_indices = np.where((wlen >= wavelen_range[0]) & (wlen <= wavelen_range[1]) & ~np.isnan(flux))
         wlen = wlen[clip_indices]
         flux = flux[clip_indices]
         errors = errors[clip_indices]
@@ -57,11 +61,13 @@ def load_crires_echelle_spectrum(data_file: str, wavelength_file: str, wavelen_r
         wlen=wlen,
         flux=flux,
         errors=errors,
+        sigma_clip=sigma_clip,
+        remove_continuum=remove_continuum,
         filter_type=filter_type,
         filter_size=filter_size,
         bp_sigma=bp_sigma,
         masked_ranges=masked_ranges,
-        metadata=metadata
+        metadata=metadata,
     )
 
 
@@ -86,6 +92,9 @@ class CRIRESEchelleDataLoader(DataLoader[CRIRESOrder]):
         merged_cfg = dict(data_cfg).copy()
         merged_cfg.update(parameters)
         
+        remove_continuum = merged_cfg.get('remove_continuum',True)
+        sigma_clip = merged_cfg.get('sigma_clip',True)
+        
         wavelen_range = merged_cfg["wavelen_range"]
         filter_size = merged_cfg["filter_size"]
         filter_type = merged_cfg["filter_type"]
@@ -109,7 +118,7 @@ class CRIRESEchelleDataLoader(DataLoader[CRIRESOrder]):
             self.write_out(f"Loading data for target {target_name} from night {n} in directory {datadir} over wavelength range {wavelen_range}")
             metadata = dict(target=target_name,display_name=disp_name,night=n,directory=datadir, datafile=data_filename,wavelength_filename=wavelength_filename)
             s = load_crires_echelle_spectrum(
-                data_file, wavelength_file, wavelen_range, filter_size, filter_type, bp_sigma, masked_ranges, metadata=metadata
+                data_file, wavelength_file, wavelen_range, sigma_clip, remove_continuum, filter_size, filter_type, bp_sigma, masked_ranges, metadata=metadata
             )
             spectra.extend(s.spectra)  # grab the orders, discard the rest
         

sika/implementations/spectroscopy/crires/crires_echelle_spectrum.py
@@ -5,7 +5,7 @@ from matplotlib.axes import Axes
 from typing import List, Tuple, Optional, Sequence
 
 from sika.implementations.spectroscopy.spectra.spectrum import Spectrum, EchelleOrder, EchelleSpectrum
-from sika.implementations.spectroscopy.utils import clean_and_continuum_subtract
+from sika.implementations.spectroscopy.utils import clean_and_continuum_subtract, clean, continuum_subtract
 
 __all__ = ["CRIRESEchelleSpectrum","CRIRESOrder"]
 
@@ -67,6 +67,8 @@ class CRIRESEchelleSpectrum(EchelleSpectrum):
 
     def __init__(self, wlen:np.ndarray, flux:np.ndarray, errors:np.ndarray,
                     order_indices:Sequence[int]=None, 
+                    sigma_clip=True,
+                    remove_continuum = True,
                     filter_type:str='median', 
                     filter_size:int=100, 
                     bp_sigma:float=3, 
@@ -84,9 +86,13 @@ class CRIRESEchelleSpectrum(EchelleSpectrum):
         :type errors: np.ndarray
         :param order_indices: autodetected if not specified. a list of lists of indices that specify which flux/wlen/error values belong in which orders.
         :type order_indices: List[Sequence[int]], optional
-        :param filter_type: _description_, defaults to 'median'
+        :param sigma_clip: whether to clean the continuum with a sigma clip of sigma=bp_sigma, defaults to True
+        :type sigma_clip: bool, optional
+        :param remove_continuum: whether to remove the continuum using the specified filter, defaults to True
+        :type remove_continuum: bool, optional
+        :param filter_type: filter type for continuum removal, defaults to 'median'
         :type filter_type: str, optional
-        :param filter_size: _description_, defaults to 100
+        :param filter_size: filter size for continuum removal, defaults to 100
         :type filter_size: int, optional
         :param bp_sigma: sigma threshold to use for outlier flux removal, defaults to 3
         :type bp_sigma: float, optional
@@ -114,10 +120,15 @@ class CRIRESEchelleSpectrum(EchelleSpectrum):
             del_mask[(wlen >= start_wlen) & (wlen <= end_wlen)] = 1
         del_mask = del_mask.astype(bool)
         
-        metadata['continuum_subtracted'] = dict(filter_type=filter_type, filter_size=filter_size, bp_sigma=bp_sigma)
-        
-        print(f"{wlen.shape = }")
-        print(f"{flux.shape = }")
+        if remove_continuum:
+            metadata['continuum_subtracted'] = dict(filter_type=filter_type, filter_size=filter_size)
+        else:
+            metadata['continuum_subtracted'] = False
+        if sigma_clip:
+            metadata['bp_sigma'] = bp_sigma
+            
+        # print(f"{wlen.shape = }")
+        # print(f"{flux.shape = }")
         
         for i, indices in enumerate(order_indices):
             o_md = metadata.copy()
@@ -125,23 +136,28 @@ class CRIRESEchelleSpectrum(EchelleSpectrum):
             wlen_order = wlen[indices]
             flux_order = flux[indices]
             error_order = errors[indices] if errors is not None else np.zeros_like(flux_order)
+            
             mask = del_mask[indices]
             if np.any(mask):
                 o_md['masked_regions_removed'] = True
             wlen_order = np.delete(wlen_order, mask)
             flux_order = np.delete(flux_order, mask)
             error_order = np.delete(error_order, mask)
-            # clean and normalize the spectrum for this order
-            flux_order, wlen_order, error_order, norm_constant = clean_and_continuum_subtract(
-                flux_order, wlen_order, error_order, filter_type=filter_type, filter_size=filter_size, bp_sigma=bp_sigma
-            )
             
+            # clean and normalize the spectrum for this order
+            if sigma_clip:
+                flux_order, wlen_order, error_order = clean(flux_order,wlen_order,error_order,bp_sigma)
+            if remove_continuum:
+                flux_order, norm_constant = continuum_subtract(flux_order, filter_size, filter_type)
+            else:
+                norm_constant = None
             o_md['norm_constant'] = norm_constant
             
             ninetieth = np.nanpercentile(flux_order,90)
             o_md['pre_norm_90th_percentile'] = ninetieth
             if ninetieth != 0:
                 flux_order /= ninetieth
+                error_order /= ninetieth
             else:
                 print(f'Warning! 90th percentile of order {i} of spectrum with metadata {metadata} is zero!')
                 
@@ -171,15 +187,15 @@ class CRIRESEchelleSpectrum(EchelleSpectrum):
         diffs = np.diff(wlen)
         diffs /= np.median(diffs)
         ind_edge = np.argwhere(diffs>100).flatten()
-        print(f"{ind_edge = }")
-        print(f"{wlen = }")
+        # print(f"{ind_edge = }")
+        # print(f"{wlen = }")
         # print('ind_edge:',ind_edge)
         if ind_edge.size > 0:
             ind_edge = np.insert(ind_edge+1, 0, 0)
             ind_edge = np.insert(ind_edge, len(ind_edge), len(wlen))
-            print('ind_edge after insert:',ind_edge)
+            # print('ind_edge after insert:',ind_edge)
             Nchip = len(ind_edge)-1
-            print(f"{Nchip = }")
+            # print(f"{Nchip = }")
             for i in range(Nchip):
                 indices.append(np.arange(ind_edge[i], ind_edge[i+1]))
         else:

sika/implementations/spectroscopy/crires/crires_spectrum.py
@@ -34,8 +34,8 @@ class CRIRESSpectrum(Spectrum):
             del_mask[(self.wlen >= start_wlen) & (self.wlen <= end_wlen)] = 1
         del_mask = del_mask.astype(bool)
         
-        print("wlen shape:",self.wlen.shape)
-        print('del_mask shape:',del_mask.shape)
+        # print("wlen shape:",self.wlen.shape)
+        # print('del_mask shape:',del_mask.shape)
 
         for indices in self.order_indices:
             wlen_order = self.wlen[indices]
@@ -43,13 +43,13 @@ class CRIRESSpectrum(Spectrum):
             error_order = self.errors[indices] if self.errors is not None else np.zeros_like(flux_order)
             mask = del_mask[indices]
             
-            print()
+            # print()
             # print("order",i)
-            print('order indices:',indices)
-            print('order indices shape:',indices.shape)
-            print('order del_mask shape:',mask.shape)
-            print('order wlen shape:',wlen_order.shape)
-            print()
+            # print('order indices:',indices)
+            # print('order indices shape:',indices.shape)
+            # print('order del_mask shape:',mask.shape)
+            # print('order wlen shape:',wlen_order.shape)
+            # print()
 
             wlen_order = np.delete(wlen_order, mask)
             flux_order = np.delete(flux_order, mask)
@@ -83,7 +83,7 @@ class CRIRESSpectrum(Spectrum):
             for i in range(Nchip):
                 indices.append(np.arange(ind_edge[i], ind_edge[i+1]))
         else:
-            print(f'else case: {np.arange(len(wlen))}')
+            # print(f'else case: {np.arange(len(self.wlen))}')
             indices.append(np.arange(len(self.wlen)))
 
         return indices

sika/implementations/spectroscopy/utils.py
@@ -295,20 +295,20 @@ def optimize_scale_factors(data_flux, data_error, model_fluxes: List[np.ndarray]
     # beta = np.sqrt(chi_squared/len(comp.norm_flux[d][c]))
     return scale_factors, beta
 
-# adapted from jerry xuan
-# filt_type -> filter_type: can be 'median' or 'gaussian'
-# medfilt -> filter_size: size of the median filter to apply
-def clean_and_continuum_subtract(fluxes, wavelengths, errors, bp_sigma=3, filter_type='median', filter_size=100):
+def clean(fluxes, wavelengths, errors, bp_sigma=3):
     clipped_indices = stats.sigma_clip(fluxes, sigma=bp_sigma)
     spike_inds = np.where(clipped_indices.mask==True)
-    # print(f"deleting {sum(len(i) for i in spike_inds)} spike points")
-    # plt.scatter(wavelengths[spike_inds], fluxes[spike_inds], s=1, color='red', label='bad pixels')
-    # plt.show()
-    # delete the nans and bad pixels
     wavelengths = np.delete(wavelengths, spike_inds)
     fluxes = np.delete(fluxes, spike_inds)
     errors = np.delete(errors, spike_inds)
     
+    return fluxes, wavelengths, errors
+
+# adapted from jerry xuan
+# filt_type -> filter_type: can be 'median' or 'gaussian'
+# medfilt -> filter_size: size of the median filter to apply
+def clean_and_continuum_subtract(fluxes, wavelengths, errors, bp_sigma=3, filter_type='median', filter_size=100):
+    fluxes, wavelengths, errors = clean(fluxes, wavelengths, errors, bp_sigma)
     f, norm_constant = continuum_subtract(fluxes, filter_size, filter_type)
     
     return f, wavelengths, errors, norm_constant